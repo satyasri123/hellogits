8edb6bd00db7a8f6f68abb2d341f8229927aac9c
vani <saiyk1555@gmail.com>
2019-04-04 15:15:45 +0530

    added second statement

--- gitProject1/src/gitPackage1/Git_Class.java
+++ gitProject1/src/gitPackage1/Git_Class.java
@@ -6,7 +6,8 @@ public class Git_Class {
 	public static void main(String[] args) {
 			//Welcome
 			System.out.println(" I love India ");
-			
+			System.out.println("Brain less");            
+		
 		}
 
 }
